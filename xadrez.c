47c76ec74f7dea58c0d98b5e7502ce682ce5cf1a
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-28 02:34:37 +0000

    criei printf

--- xadrez.c
+++ xadrez.c
@@ -6,6 +6,9 @@
 
 int main() {
     // Nível Novato - Movimentação das Peças
+
+    prinf("Desafio xadrez!\n");
+    
     int / Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
     // Implementação de Movimentação do Bispo
